a64a25cb6b1ad5d70dd2b523e479e3e24f520094
Tim Pezdirc <Tim.Pezdirc@gmail.com>
2023-02-26 15:11:46 +0100

    Dodaj izračun pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Gravitacija {
 
     public static void main(String[] args) {
         
-        System.out.println("OIS je zakon!");
+        Scanner sc = new Scanner(System.in);
+        int nadmorskaVisina = sc.nextInt();
 
+        final double C = 6.674e-11;
+        final double M = 5.972e24;
+        final double r = 6.371e6;
+
+    }
+
+    public static double izracun(int visina, double C, double M, double r) {
+        return (C * M) / (Math.pow(r + visina, 2));
     }
 
 }
